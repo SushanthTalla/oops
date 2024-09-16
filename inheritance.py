33a6e2e6fe78c8b7d8de30f669348ab0e2233e09
SushanthTalla <tallasushanth@gmail.com>
2024-09-16 23:56:50 +0530

    oops concept

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+class sushanth:
+    def __init__(self):
+        self.data = ['height','weight','color']
+
+
+class charan(sushanth):
+    def data(self):
+        return self.data
+
+obj=charan()
+print(obj.data)   
